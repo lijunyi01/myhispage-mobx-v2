16fc9fbc5a1b77d2b350cc2bc1a04733ddbd4a40
lijunyi01 <lijunyi01@gmail.com>
2021-01-17 11:58:18 +0800

    init modify

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
 
 function App() {
   return (
     <div className="App">
+      <Button type="primary">按钮</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
