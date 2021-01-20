4e2008296815ee944df450607f25e65e7ae7ce12
lijunyi01 <lijunyi01@gmail.com>
2021-01-20 23:11:43 +0800

    modify

--- src/pages/Home/TimeLine/index.js
+++ src/pages/Home/TimeLine/index.js
@@ -1,15 +1,57 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { observer } from 'mobx-react';
 import { Spin, Button } from 'antd';
 import timeLineState from './index.state'
 import { MainList, MainContent } from './components';
+import _ from 'lodash';
 import './index.less';
 
 const Index = (props) => {
 
-    // useEffect(() => {
-    //     state.changetest();
-    // }, [])
+    const maincontentdivref = useRef(null);
+
+    const setCanvasWidth = _.debounce(() => {
+
+        console.log("setCanvasWidth");
+        let divdom = maincontentdivref.current;
+        if (divdom) {
+            let mainContentDivWidth = divdom.offsetWidth;
+            console.log("domWidth:" + mainContentDivWidth);
+            //     //第一次加载会多算80px（每边24+16）
+            //     if(this.isFirstLoaded) {
+            //         mainContentDivWidth -= 80;
+            //         this.isFirstLoaded = false;
+            //     }
+            //     if (mainContentDivWidth !== this.state.mainContentDivWidth) {
+            //         this.setState({
+            //             mainContentDivWidth: mainContentDivWidth
+            //         })
+            //     }
+        }
+    }, 200);
+
+    useEffect(() => {
+        console.log("add listener");
+        window.addEventListener(
+            'resize',
+            // ()=>{
+            //     this.setCanvasWidth()
+            // },
+            // 这里的处理函数不能用匿名函数，否则removeEventListener 会无效！
+            setCanvasWidth,
+            false
+        );
+        setCanvasWidth();
+        // 如果 effect 返回一个函数，React 将会在执行清除操作时调用它；返回的可以是匿名函数
+        return function cleanup() {
+            console.log("clean listener");
+            window.removeEventListener(
+                'resize',
+                setCanvasWidth,
+                false
+            );
+        };
+    }, []);
 
     return (
         <div id="wrap">
@@ -19,7 +61,7 @@ const Index = (props) => {
             <div className="mainlist">
                 <MainList />
             </div>
-            <div id="maincontent">
+            <div ref={maincontentdivref} id="maincontent">
                 <MainContent mainContentDivWidth={300} />
             </div>
         </div>
