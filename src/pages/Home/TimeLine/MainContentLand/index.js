b1a5cb8a3aa06ced67381ab58fcc8bbe1baaebac
lijunyi01 <lijunyi01@gmail.com>
2021-02-08 22:26:18 +0800

    modify

--- src/pages/Home/TimeLine/MainContentLand/index.js
+++ src/pages/Home/TimeLine/MainContentLand/index.js
@@ -17,84 +17,97 @@ function Index(props) {
 
     let { lastYear, earlyYear, pxPerYear, yearInterval, timeLineBeginYear } = canvasParam;
     let canvasWidth = (lastYear - earlyYear) * pxPerYear < 50 ? 300 : (lastYear - timeLineBeginYear + yearInterval) * pxPerYear + 50;
-    // console.log("canvasParam:", canvasParam);
-    // console.log("canvasHeight:", canvasHeight);
 
     // 计算卡片左上角的x坐标（相对于id为 maincontent的父级div）
     let itemInMainParam = {};
-    const getLeftPos = (index, topPos, timeLineBeginYear, pxPerYear) => {
 
-        // let itemList = containerState.projectContents[projectId];
+    // const getLeftPos2 = (index, topPos, timeLineBeginYear, pxPerYear) => {
 
-        // 列表区右侧整个区域的宽度，canvas只是其中的30%宽度
-        let { mainContentDivWidth } = props
+    //     // let itemList = containerState.projectContents[projectId];
 
-        let leftPos;
+    //     // 列表区右侧整个区域的高度，canvas只是其中的30%宽度
+    //     let { mainContentDivHeight } = props
 
-        if (index % 2 === 0) {
-            leftPos = 40;
-            if (index >= 2) {
-                if (topPos === itemInMainParam[index - 2].topPos) {
-                    leftPos = itemInMainParam[index - 2].leftPos - 15;
+    //     let leftPos;
 
-                } else if (Math.abs(topPos - itemInMainParam[index - 2].topPos) < 90) {
-                    if (itemInMainParam[index - 2].leftPos === 40) {
-                        leftPos = 50;
-                    }
-                }
+    //     if (index % 2 === 0) {  // 偶数项，在下边
+    //         leftPos = 40;
+    //         if (index >= 2) {
+    //             if (topPos === itemInMainParam[index - 2].topPos) {
+    //                 leftPos = itemInMainParam[index - 2].leftPos - 15;
 
-            }
-        } else {
-            // leftPos = canvasWidth/0.3*0.65;
-            leftPos = mainContentDivWidth * 0.65;
-            if (index >= 2) {
-                if (topPos === itemInMainParam[index - 2].topPos) {
-                    leftPos = itemInMainParam[index - 2].leftPos + 15;
+    //             } else if (Math.abs(topPos - itemInMainParam[index - 2].topPos) < 90) {
+    //                 if (itemInMainParam[index - 2].leftPos === 40) {
+    //                     leftPos = 50;
+    //                 }
+    //             }
 
-                } else if (Math.abs(topPos - itemInMainParam[index - 2].topPos) < 90) {
-                    if (itemInMainParam[index - 2].leftPos === leftPos) {
-                        leftPos = leftPos - 10;
-                    }
-                }
+    //         }
+    //     } else {  // 奇数项，在上边
+    //         // leftPos = canvasWidth/0.3*0.65;
+    //         leftPos = mainContentDivWidth * 0.65;
+    //         if (index >= 2) {
+    //             if (topPos === itemInMainParam[index - 2].topPos) {
+    //                 leftPos = itemInMainParam[index - 2].leftPos + 15;
 
-            }
-        }
-        return leftPos;
-    }
+    //             } else if (Math.abs(topPos - itemInMainParam[index - 2].topPos) < 90) {
+    //                 if (itemInMainParam[index - 2].leftPos === leftPos) {
+    //                     leftPos = leftPos - 10;
+    //                 }
+    //             }
 
-    // 计算卡片左上角的y坐标（相对于id为 maincontent的父级div）
-    const getTopPos = (index, timeLineBeginYear, pxPerYear) => {
+    //         }
+    //     }
+    //     return leftPos;
+    // }
+
+    const getLeftPos = (index, timeLineBeginYear, pxPerYear) => {
 
         // let { containerState } = this.props;
-        let marginTop = 30;
-        let topPos = marginTop;
+        let marginLeft = 30;
+        let leftPos = marginLeft;
 
         let { projectItems } = canvasParam;
 
         if (projectItems[index].itemType < 3) {    //点事件
-            topPos = (projectItems[index].startYear - timeLineBeginYear) * pxPerYear - 40 + marginTop;
+            leftPos = (projectItems[index].startYear - timeLineBeginYear) * pxPerYear - 40 + marginLeft;
         } else {       //段事件
-            let rectTopY = (projectItems[index].startYear - timeLineBeginYear) * pxPerYear + marginTop;
-            let rectHeight = (projectItems[index].endYear - projectItems[index].startYear) * pxPerYear;
-            topPos = rectTopY + rectHeight / 2 - 40;
+            let rectLeftX = (projectItems[index].startYear - timeLineBeginYear) * pxPerYear + marginLeft;
+            let rectWidth = (projectItems[index].endYear - projectItems[index].startYear) * pxPerYear;
+            leftPos = rectLeftX + rectWidth / 2 - 40;
         }
 
+        return leftPos;
+    }
+
+    // 计算卡片左上角的y坐标（相对于id为 maincontent的父级div）
+    const getTopPos = (index, leftPos, timeLineBeginYear, pxPerYear) => {
+
+        const centerY = props.mainContentDivHeight / 2;
+        const canvasContainerHeight = props.mainContentDivHeight * 0.3;
+
+        let topPos = 150;
+        if (index % 2 === 0) {  // 偶数项，在下边
+            topPos = centerY + canvasContainerHeight / 2 - 2;
+        } else {             // 奇数项，在上边
+            topPos = centerY - canvasContainerHeight / 2 - 80;
+        }
         return topPos;
     }
 
     return (
         canvasWidth ?
             <div className="card-canvas-container">
-                {/* {
+                {
                     toJS(timeLineState.activedProjectItems).map((item, index) => {
                         let topPos = 0;
-                        topPos = getTopPos(index, timeLineBeginYear, pxPerYear);
                         let leftPos = 0;
-                        leftPos = getLeftPos(index, topPos, timeLineBeginYear, pxPerYear);
+                        leftPos = getLeftPos(index, timeLineBeginYear, pxPerYear);
+                        topPos = getTopPos(index, leftPos, timeLineBeginYear, pxPerYear);
                         itemInMainParam[index] = { 'topPos': topPos, 'leftPos': leftPos };
                         return <ProjItemCard key={item.itemId} leftPos={leftPos} topPos={topPos} cardParam={item} />
                     })
-                } */}
+                }
 
                 < div className="canvas-container" style={{ width: canvasWidth + 10 }}>
                     <MyCanvas {...canvasParam} canvasWidth={canvasWidth} canvasHeight={props.mainContentDivHeight * 0.3} />
