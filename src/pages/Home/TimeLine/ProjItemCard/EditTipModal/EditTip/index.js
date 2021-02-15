00a351013c96475b2e3ab48d5a697450bc42da5f
lijunyi01 <lijunyi01@gmail.com>
2021-02-15 23:06:09 +0800

    modify

--- src/pages/Home/TimeLine/ProjItemCard/EditTipModal/EditTip/index.js
+++ src/pages/Home/TimeLine/ProjItemCard/EditTipModal/EditTip/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { toJS } from 'mobx';
 import timeLineState from '@pages/Home/TimeLine/index.state';
+import { Table, Button, Form, Input, Divider } from 'antd';
 
 function index(props) {
 
@@ -24,15 +25,76 @@ function index(props) {
     const itemTips = getItemTips();
     console.log("tips:", itemTips);
 
+    const columns = [
+        {
+            title: 'Tip',
+            dataIndex: 'tipcontent',
+            key: 'tipcontent',
+            width: 370,
+        },
+        {
+            title: 'Action',
+            key: 'action',
+            render: (text, record) => (
+                <Button type="primary" onClick={() => { console.log("record:", record); }}>Delete</Button>
+            ),
+        },
+    ];
+
+    const formItemLayout = {
+        labelCol: {
+            xs: {
+                span: 18,
+            },
+            sm: {
+                span: 6,
+            },
+        },
+        wrapperCol: {
+            xs: {
+                span: 24,
+            },
+            sm: {
+                span: 16,
+            },
+        },
+    };
+    const formItemTailLayout = {
+        wrapperCol: { offset: 6, span: 16 },
+    };
+
+    const onFinish = values => { }
+
     return (
         <div>
-            {itemTips.length > 0 ?
-                itemTips.map(item => {
-                    return <div>{item.tipcontent}</div>
-                })
-                :
-                <div>空</div>
-            }
+            <Table dataSource={itemTips} rowKey="id" columns={columns} bordered scroll={{ y: 150 }} pagination={false} />
+            <Divider />
+            <Form
+                name="new project"
+                initialValues={{}}
+                {...formItemLayout}
+                onFinish={onFinish}
+            >
+                <Form.Item
+                    name="tipContent"
+                    label="tip content:"
+                    rules={[{ required: true, message: 'Please input tip content!' }]}
+                >
+                    <Input />
+                </Form.Item>
+                {/* <Form.Item
+                    name="projectDes"
+                    label="project des:"
+                    rules={[{ required: true, message: 'Please input project des!' }]}
+                >
+                    <Input />
+                </Form.Item> */}
+                <Form.Item {...formItemTailLayout}>
+                    <Button type="primary" htmlType="submit" style={{ 'width': '50%', 'float': 'right' }}>
+                        新增Tip
+                    </Button>
+                </Form.Item>
+            </Form>
         </div>
     )
 }
