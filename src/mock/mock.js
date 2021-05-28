dcda86a410821adb67950755fa1c471bccf72c17
lijunyi01 <lijunyi01@gmail.com>
2021-05-28 09:45:44 +0800

    mock data

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,4 +1,5 @@
 import Mock from 'mockjs'
+import mockData from './mockData'
 
 let mockInfo = {
     "code": 0,
@@ -14,6 +15,7 @@ let mockInfo2 = {
     }
 }
 
+// 模拟1秒的通讯延迟
 Mock.setup({
     timeout: 1000
 })
@@ -30,31 +32,32 @@ Mock.mock(
     }
 )
 
-let loginSuccessResp = {
-    "messageCode": "S100001",
-    "status": 0,
-    "token": "eyJhbGciOiJIUzUxMiJ9.eyJ1bWlkIjoiMSIsImNyZWF0ZWQiOjE2MjIwMjg5MzAwODcsInRhYmxlaW5kZXgiOiIwIiwiZXhwIjoxNjIyNjMzNzMwfQ.86mTj2Rbo3sAO4HEATHiIXfqvZ7DYLZD7jkAN2wk68rKsjiOytWvdIxGoVkLwCFZ47p6Ig1XfFkV5FANKwl88g",
-    "umid": 1
-}
+// let loginSuccessResp = {
+//     "messageCode": "S100001",
+//     "status": 0,
+//     "token": "eyJhbGciOiJIUzUxMiJ9.eyJ1bWlkIjoiMSIsImNyZWF0ZWQiOjE2MjIwMjg5MzAwODcsInRhYmxlaW5kZXgiOiIwIiwiZXhwIjoxNjIyNjMzNzMwfQ.86mTj2Rbo3sAO4HEATHiIXfqvZ7DYLZD7jkAN2wk68rKsjiOytWvdIxGoVkLwCFZ47p6Ig1XfFkV5FANKwl88g",
+//     "umid": 1
+// }
 
-let loginFailResp = {
-    "messageCode": "E100002",
-    "status": 20,
-    "token": "",
-    "umid": 0
-}
+// let loginFailResp = {
+//     "messageCode": "E100002",
+//     "status": 20,
+//     "token": "",
+//     "umid": 0
+// }
 
 // /auth/login
 Mock.mock(
-    '/auth/login2',   // 如要让mock拦截请求，将url改为匹配的url即可
+    '/auth/login',   // 如要让mock拦截请求，将url改为匹配的url即可
     'post',
     data => {
         let params = JSON.parse(data.body)
         console.log("mock data:", params)
         if (params.loginPwd === "111111") {
-            return loginSuccessResp
+            return mockData.loginSuccessResp
         } else {
-            return loginFailResp
+            // return loginFailResp
+            return mockData.loginFailResp
         }
 
     }
---
+++ src/mock/mockData.js
@@ -0,0 +1,17 @@
+const mockData = {
+    loginSuccessResp: {
+        "messageCode": "S100001",
+        "status": 0,
+        "token": "eyJhbGciOiJIUzUxMiJ9.eyJ1bWlkIjoiMSIsImNyZWF0ZWQiOjE2MjIwMjg5MzAwODcsInRhYmxlaW5kZXgiOiIwIiwiZXhwIjoxNjIyNjMzNzMwfQ.86mTj2Rbo3sAO4HEATHiIXfqvZ7DYLZD7jkAN2wk68rKsjiOytWvdIxGoVkLwCFZ47p6Ig1XfFkV5FANKwl88g",
+        "umid": 1
+    },
+
+    loginFailResp: {
+        "messageCode": "E100002",
+        "status": 20,
+        "token": "",
+        "umid": 0
+    }
+};
+
+export default mockData;
